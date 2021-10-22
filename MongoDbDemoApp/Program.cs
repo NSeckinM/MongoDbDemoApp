3385f1ebf336a849c61f1cc9a6cd5ef736fea9ea
NseckinM <seckinmantar@gmail.com>
2021-10-23 01:05:40 +0300

    Add project files.

---
+++ MongoDbDemoApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MongoDbDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            Console.ReadLine();
+
+        }
+    }
+}
